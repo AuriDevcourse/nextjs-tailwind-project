e5d1b0267aa90ac54c507b1ced3bb2b24ee75592
Aurimas Baciauskas <aurimasbaciauskas@Aurimass-MacBook-Pro.local>
2024-09-21 11:39:28 +0200

    Initial commit

---
+++ root/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="container mx-auto p-6 bg-red-500 text-white">
+      <h1 className="text-4xl font-bold text-center mb-12">
+        Hello, world! Welcome to my Next.js project!
+      </h1>
+    </div>
+  );
+}
